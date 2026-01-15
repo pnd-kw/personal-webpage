8ba3f6934e160ae36d3ba0025ddc1745fd9b5663
Pandu Office PC <pandu.usahaku@gmail.com>
2026-01-15 13:23:21 +0700

    fix: image does not load

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
+  basePath: isProd ? "/personal-webpage" : "",
+  assetPrefix: isProd ? "/personal-webpage/" : "",
   images: {
     unoptimized: true,
   },
